d49f4c0ebfbb5285a3aad8df9f876e25b7afea94
Vladimir Vavilin <vavilinva59@gmail.com>
2022-06-30 12:19:42 +0500

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Book.java
@@ -0,0 +1,27 @@
+package ru.job4j.pojo;
+
+public class Book {
+    public String title;
+    public int pages;
+
+    public Book(String title, int pages) {
+        this.title = title;
+        this.pages = pages;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,30 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book invincible = new Book("Invincible", 224);
+        Book foundation = new Book("Foundation", 320);
+        Book solaris = new Book("Solaris", 288);
+        Book cleanCode = new Book("Clean code", 464);
+        Book[] library = new Book[4];
+        library[0] = invincible;
+        library[1] = foundation;
+        library[2] = solaris;
+        library[3] = cleanCode;
+        for (int index = 0; index < library.length; index++) {
+            Book book = library[index];
+            System.out.println(book.getTitle() + " - " + book.getPages());
+        }
+        library[0] = cleanCode;
+        library[3] = invincible;
+        for (int index = 0; index < library.length; index++) {
+            System.out.println(library[index].getTitle() + " - " + library[index].getPages());
+        }
+        for (int index = 0; index < library.length; index++) {
+            if (library[index].equals(cleanCode)) {
+                System.out.println(library[index].getTitle() + " - " + library[index].getPages());
+            }
+        }
+
+    }
+}
