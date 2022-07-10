76ec92bdecd65f4c0e6687a79ad7c4d73af36979
Vladimir Vavilin <vavilinva59@gmail.com>
2022-07-10 15:26:22 +0500

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -55,4 +55,15 @@ public class Tracker {
         }
         return rsl;
     }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        boolean rsl = index != -1;
+        if (rsl) {
+            items[index] = null;
+            System.arraycopy(items, index + 1, items, index, size - 1);
+            size--;
+        }
+        return rsl;
+    }
 }
