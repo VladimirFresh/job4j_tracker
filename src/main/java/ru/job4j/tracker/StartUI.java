8df3485209e260d40ffe8101569387a7f7c45c32
Vladimir Vavilin <vavilinva59@gmail.com>
2022-07-10 21:58:54 +0500

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -10,5 +10,6 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTimeFormat = createdFormat.format(formatter);
         System.out.println(currentDateTimeFormat);
+        System.out.println(item);
     }
 }
