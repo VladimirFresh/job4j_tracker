f4c993f143e1d666b5cd2333abde1fe0d23376e9
Vladimir Vavilin <vavilinva59@gmail.com>
2023-06-21 13:29:00 +0500

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,8 @@ public class UsageMap {
         HashMap<String, String> map = new HashMap<>();
         map.put("Vasily Pupkin", "vasilypupkin@yahoo.com");
         map.put("Unknown User", "unknownuser@yahoo.com");
+        map.put("Unknown User", "anothermail@yahoo.com");
+        map.put("Vasily Pupkin", "moremails@yahoo.com");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
